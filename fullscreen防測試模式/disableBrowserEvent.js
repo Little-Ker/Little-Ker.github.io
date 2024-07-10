d9af99c93a3ba43ed80df8e534bba3b177cc9c57
Vivi <vivichi@yile.com.tw>
2024-07-10 16:15:24 +0800

    [新增] 防測試模式

---
+++ fullscreen防測試模式/disableBrowserEvent.js
@@ -0,0 +1,28 @@
+// 禁止右键
+window.oncontextmenu=function(){
+  console.log('right click')
+  return false;
+}
+
+// 禁止鍵盤事件(防F12)
+window.onkeydown = window.onkeyup = window.onkeypress = function () {
+  console.log('key')
+  window.event.returnValue = false;
+  return false;
+}
+
+// 檢查到 debugMode 就無限跳中斷點
+setInterval(function() {
+  checkDebugMode();
+}, 4000);
+var checkDebugMode = function() {
+  function doCheck() {
+     (function() {}["constructor"]("debugger")())
+     doCheck()
+  }
+  try {
+     doCheck()
+  }
+  catch (err) {}
+};
+checkDebugMode();
---
+++ fullscreen防測試模式/fullscreen.js
@@ -0,0 +1,105 @@
+const gameDetail = document.querySelector('#gameDetail');
+const gameCanvas = document.querySelector('#gameCanvas');
+const game = document.querySelector('#game');
+const gameBg = document.querySelector('#gameBg');
+
+const scrollBg = document.querySelector('#scrollBg');
+
+const tipText = document.querySelector('#tipText');
+
+// 是否關閉提示
+const setIsCloseTip = (close) => {
+    if (close) {
+        tipText.innerText = '';
+        tipText.classList.add('closeTip');
+    } else {
+        tipText.innerText = '手機請轉直';
+        tipText.classList.remove('closeTip');
+    }
+}
+
+// 是否關閉遊戲背景
+const setIsGameBgClose = (close) => {
+    if (close) {
+        gameBg.classList.add('closeGameBg');
+    } else {
+        gameBg.classList.remove('closeGameBg');
+    }
+}
+
+// 手機橫直向判斷
+const orientation = () => {
+    if (!screenDir.matches) {
+        console.log('目前是橫向')
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        document.body.style.overflow = 'hidden';
+        setIsCloseTip(false);
+    } else {
+        console.log('目前是直向')
+        document.body.style.overflow = 'auto';
+        setIsCloseTip(true);
+    }
+}
+const screenDir = window.matchMedia("(orientation: portrait)");
+screenDir.addListener(orientation);
+
+// 裝置判斷
+const mobile = () => {
+    try {
+        document.createEvent('TouchEvent');
+        console.log('手機裝置')
+        return true;
+    } catch (e) {
+        console.log('電腦裝置')
+        return false;
+    }
+}
+
+// 遊戲畫面大小調整(根據高度縮放)
+const resize = () => {
+    if (!gameCanvas) return;
+    const pageW = 720;
+    const pageH = 1280;
+    const ratio = pageH / pageW;
+
+    const winW = window.innerWidth;
+    const winH = (window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight);
+
+    let width = winH / ratio;
+    let height = winH;
+
+    if (height / winW >= ratio) {
+        height = winW * ratio;
+        width = winW;
+    }
+
+    gameCanvas.style.width = `${width || 0}px`;
+    gameCanvas.style.height = `${height || 0}px`;
+
+    if (mobile())  {
+        scrollBg.style.height = `${height + 600}px`;
+    }
+}
+
+window.addEventListener('resize', () => {
+    if (mobile()) {
+        setIsGameBgClose(true)
+        orientation();
+    } else {
+        setIsGameBgClose(false)
+        setIsCloseTip(true)
+    }
+    resize();
+});
+
+window.onload = () => {
+    if (mobile()) {
+        setIsGameBgClose(true)
+        orientation();
+    } else {
+        setIsGameBgClose(false)
+        setIsCloseTip(true)
+    }
+    resize();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
