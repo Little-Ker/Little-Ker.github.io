578029f122279948c6eee5b869aa40cf40098db4
Vivi <vivichi@yile.com.tw>
2024-07-10 10:46:03 +0800

    一步一步慢慢試

--- fullscreen下面留一塊/fullscreen.js
+++ fullscreen下面留一塊/fullscreen.js
@@ -6,8 +6,68 @@ const gameDetail = document.querySelector('#gameDetail');
         const scrollBg = document.querySelector('#scrollBg');
 
         const tipText = document.querySelector('#tipText');
+        const fullScreenBtn = document.querySelector('#fullScreenBtn');
 
-        const btnList = document.querySelector('#btnList');
+        let isFullScreen = false;
+
+
+        // 開啟 遊戲全屏畫面
+        const openFullScreenBtn = () => {
+            fullScreenBtn.innerText = 'Exit Full Screen';
+            isFullScreen = true
+            if (mobile()) {
+                resize();
+                setFullScreenGame(true);
+                return
+            }
+
+            // 網頁版 F11 放大畫面
+            if (gameDetail?.requestFullscreen) {
+                gameDetail.requestFullscreen();
+            } else if (gameDetail?.webkitRequestFullscreen) { // Safari 和 Chrome
+                gameDetail.webkitRequestFullscreen();
+            } else if (gameDetail?.mozRequestFullScreen) { // Firefox
+                gameDetail.mozRequestFullScreen();
+            } else if (gameDetail?.msRequestFullscreen) { // IE/Edge
+                gameDetail.msRequestFullscreen();
+            } else {
+                console.log("Fullscreen API is not supported.");
+            }
+        }
+
+        // 關閉 遊戲全屏畫面
+        const closeFullScreenBtn = () => {
+            fullScreenBtn.innerText = 'Full Screen';
+            isFullScreen = false
+            if (mobile()) {
+                resize();
+                setFullScreenGame(false);
+                return
+            }
+            document.exitFullscreen();
+        }
+
+        const onFullScreen = () => {
+            if (isFullScreen) {
+                closeFullScreenBtn()
+                return
+            }
+            openFullScreenBtn()
+        }
+
+        fullScreenBtn.addEventListener('click', onFullScreen);
+
+        // 遊戲是否顯示全屏
+        const setFullScreenGame = (show) => {
+            console.log('show',show)
+            if (show) {
+                gameDetail.classList.add('gameDetailMax');
+                game.classList.add('gameMax');
+            } else {
+                gameDetail.classList.remove('gameDetailMax');
+                game.classList.remove('gameMax');
+            }
+        }
 
         // 是否關閉提示
         const setIsCloseTip = (close) => {
@@ -64,10 +124,11 @@ const gameDetail = document.querySelector('#gameDetail');
             const pageH = 1280;
             const ratio = pageH / pageW;
 
+            const navbarHeight = 0;
             const fullScreenBtnHeight = 36
 
             const winW = window.innerWidth;
-            const winH = (window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight) - fullScreenBtnHeight;
+            const winH = (window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight) - fullScreenBtnHeight - (isFullScreen ? 0 : navbarHeight);
 
             let width = winH / ratio;
             let height = winH;
@@ -79,8 +140,6 @@ const gameDetail = document.querySelector('#gameDetail');
 
             gameCanvas.style.width = `${width || 0}px`;
             gameCanvas.style.height = `${height || 0}px`;
-
-            btnList.innerText = `寬: ${width} 高: ${height}`;
 
             if (mobile())  {
                 scrollBg.style.height = `${height + 600}px`;
